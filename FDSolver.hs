6b94cf6f867a065532470ba1636111d27c2eb4a3
Tom Hawkins <tomahawkins@gmail.com>
2016-11-29 20:44:44 -0500

    FD solver.

---
+++ FDSolver.hs
@@ -0,0 +1,137 @@
+{-# LANGUAGE GADTs #-}
+
+-- | A finite domain constraint solver.
+module FDSolver
+  ( FD
+  , Var (..)
+  , E (..)
+  , solve
+  , solve'
+  , newVar
+  , assert
+  ) where
+
+import Data.List
+import MonadLib
+
+-- | The FD monad.
+type FD a = StateT (FDDB a) Id
+
+data FDDB a = FDDB
+  { nextId      :: Int
+  , variables   :: [[a]]  -- Vars index into list.
+  , constraints :: [E]
+  }
+
+-- | Variable.
+data Var = Var Int deriving Eq
+instance Show Var where show (Var i) = show i
+
+-- | Constraint expressions.
+data E where
+  (:&&) :: E -> E -> E
+  (:||) :: E -> E -> E
+  (:->) :: E -> E -> E
+  (:==) :: Var -> Var -> E
+  (:/=) :: Var -> Var -> E
+  (:$=) :: [Var] -> [Var] -> E
+  deriving Show
+
+infix  4 :==, :/=, :$=
+infixl 3 :&&
+infixl 2 :||
+infixr 1 :->
+
+-- | Solve a set of constraints and update a structure with solved domain values.
+solve :: Ord a => FD a b -> (b, Var -> [a])
+solve fd =  (b, \ (Var i) -> solvedVars !! i)
+  where
+  (b, db) = runId $ runStateT (FDDB 0 [] []) fd
+  solvedVars = applyConstraints (constraints db) $ variables db
+
+solve' :: Ord a => FD a b -> (b, Var -> (Var, [a]), [E])
+solve' fd =  (b, \ (Var i) -> (Var i, solvedVars !! i), constraints db)
+  where
+  (b, db) = runId $ runStateT (FDDB 0 [] []) fd
+  solvedVars = applyConstraints (constraints db) $ variables db
+
+applyConstraints :: Ord a => [E] -> [[a]] -> [[a]]
+applyConstraints constraints vars
+  | vars == vars' = vars
+  | otherwise     = applyConstraints constraints vars'
+  where
+  vars' = foldr applyConstraint vars constraints
+
+applyConstraint :: Ord a => E -> [[a]] -> [[a]]
+applyConstraint a vars = case a of
+  a :$= b -> foldr (\ (Var a) vars -> replace ((vars !! a) `intersect` vars') a vars) vars (a ++ b)
+    where
+    vars' = (nub $ concat [ vars !! a | Var a <- a ]) `intersect` (nub $ concat [ vars !! b | Var b <- b ])
+  Var a :== Var b -> replace n a $ replace n b vars where n = (vars !! a) `intersect` (vars !! b)
+  Var a :/= Var b
+    | length (vars !! a) == 1 -> replace ((vars !! b) \\ (vars !! a)) b vars
+    | length (vars !! b) == 1 -> replace ((vars !! a) \\ (vars !! b)) a vars
+    | otherwise -> vars
+  a :-> b -> case eval vars a of
+    Nothing    -> vars
+    Just False -> vars
+    Just True  -> applyConstraint b vars
+  a :&& b -> applyConstraint a $ applyConstraint b vars
+  _ :|| _
+    | length terms == length falses + 1 -> applyConstraint term vars
+    | otherwise -> vars
+    where
+    terms  = [ (t, eval vars t) | t <- orTerms a ]
+    falses = [ () | (_, Just False) <- terms ]
+    term   = head [ a | (a, v) <- terms, v /= Just False ]
+  where
+  replace a i l = take i l ++ [a] ++ drop (i + 1) l
+  orTerms a = case a of
+    a :|| b -> orTerms a ++ orTerms b
+    a       -> [a]
+
+eval :: Ord a => [[a]] -> E -> Maybe Bool
+eval vars a = case a of
+  a :&& b -> case (eval vars a, eval vars b) of
+    (Just True, Just True) -> Just True
+    (Just False, _) -> Just False
+    (_, Just False) -> Just False
+    _ -> Nothing
+  a :|| b -> case (eval vars a, eval vars b) of
+    (Just False, Just False) -> Just False
+    (Just True, _) -> Just True
+    (_, Just True) -> Just True
+    _ -> Nothing
+  a :-> b -> case eval vars a of
+    Nothing -> Nothing
+    Just True -> eval vars b
+    Just False -> Just True
+  Var a :== Var b
+    | length (vars !! a) == 1 && length (vars !! b) == 1 -> Just (vars !! a == vars !! b)
+    | null ((vars !! a) `intersect` (vars !! b)) -> Just False
+    | otherwise -> Nothing
+  Var a :/= Var b
+    | length (vars !! a) == 1 && length (vars !! b) == 1 -> Just (vars !! a /= vars !! b)
+    | null ((vars !! a) `intersect` (vars !! b)) -> Just True
+    | otherwise -> Nothing
+  a :$= b
+    | all (\ a -> length a == 1) (a' ++ b') && length a == length b -> Just (sort (concat a') == sort (concat b'))
+    | otherwise -> Nothing
+    where
+    a' = [ vars !! a | Var a <- a]
+    b' = [ vars !! b | Var b <- b]
+
+-- | Create a new variables with an initial domain.
+newVar :: [a] -> FD a Var
+newVar domain = do
+  if null domain then error "Variables can't have empty domain." else return ()
+  db <- get
+  set db { nextId = nextId db + 1, variables = variables db ++ [domain] }
+  return $ Var $ nextId db
+
+-- | Add a constraint.
+assert :: E -> FD a ()
+assert a = do
+  db <- get
+  set db { constraints = constraints db ++ [a] }
+
--- Sudoku.hs
+++ Sudoku.hs
@@ -1,22 +1,115 @@
 module Main (main) where
 
+import Data.List (transpose)
+
 import Board
-import Sections
+import FDSolver hiding (solve)
+import qualified FDSolver as FD
+import Sections (solve)
 
 main :: IO ()
 main = do
-  solve' easy
-  solve' med29
-  solve' med30
-  solve' hard111
-  solve' hard112
+  solve1 easy
+  solve1 med29
+  solve1 med30
+  solve1 hard111
+  solve1 hard112
+  solve1 garfield3
+  solve1 garfield4
 
-solve' :: Board -> IO ()
-solve' a = do
+solve1 :: Board -> IO ()
+solve1 a = do
   putStrLn $ unlines [ "    " ++ a ++ "    " ++ b | (a, b) <- zip (lines $ show a) (lines $ show b) ]
   putStrLn ""
   where
+  --b = solveBoard a
   b = solve a
+
+solveBoard :: Board -> Board
+solveBoard a = toBoard $ FD.solve $ do
+  a <- fromBoard a
+  mapM_ allConstraints a
+  mapM_ allConstraints $ transpose a
+  setConstraints $ sections a
+  return a
+
+sections :: [[a]] -> [[ [[a]] ]]
+sections a = [[s00, s01, s02], [s10, s11, s12], [s20, s21, s22]]
+  where
+  r0  = transpose $ take 3 a
+  r1  = transpose $ take 3 $ drop 3 a
+  r2  = transpose $ drop 6 a
+  s00 = transpose $ take 3          r0
+  s01 = transpose $ take 3 $ drop 3 r0
+  s02 = transpose $          drop 6 r0
+  s10 = transpose $ take 3          r1
+  s11 = transpose $ take 3 $ drop 3 r1
+  s12 = transpose $          drop 6 r1
+  s20 = transpose $ take 3          r2
+  s21 = transpose $ take 3 $ drop 3 r2
+  s22 = transpose $          drop 6 r2
+
+fromBoard :: Board -> FD Int [[Var]]
+fromBoard (Board a) = mapM (mapM $ \ a -> case a of { Nothing -> newVar [1 .. 9]; Just i -> newVar [i] }) a
+
+toBoard :: ([[Var]], Var -> [Int]) -> Board
+toBoard (a, f) = Board $ map (map f') a
+  where
+  f' a = if length (f a) == 1 then Just $ head $ f a else Nothing
+
+allConstraints a = do
+  allDifferent a
+  --allAccounted a
+
+allDifferent :: [Var] -> FD Int ()
+allDifferent a = case a of
+  [] -> return ()
+  a : b -> do
+    sequence_ [ assert $ a :/= b | b <- b ]
+    allDifferent b
+
+setConstraints :: [[ [[Var]] ]] -> FD Int ()
+setConstraints [[s00, s01, s02], [s10, s11, s12], [s20, s21, s22]] = do
+  --mapM_ (allDifferent . concat) [s00, s01, s02, s10, s11, s12, s20, s21, s22]
+  setConstraintsRow s00 s01 s02
+  setConstraintsRow s10 s11 s12
+  setConstraintsRow s20 s21 s22
+  setConstraintsRow (transpose s00) (transpose s10) (transpose s20)
+  setConstraintsRow (transpose s01) (transpose s11) (transpose s21)
+  setConstraintsRow (transpose s02) (transpose s12) (transpose s22)
+setConstraints _ = undefined
+
+setConstraintsRow :: [[Var]] -> [[Var]] -> [[Var]] -> FD Int ()
+setConstraintsRow [a0, a1, a2] [b0, b1, b2] [c0, c1, c2] = do
+  assert $ (a1 ++ a2) :$= (b0 ++ c0)
+  assert $ (a0 ++ a2) :$= (b1 ++ c1)
+  assert $ (a0 ++ a1) :$= (b2 ++ c2)
+  assert $ (b1 ++ b2) :$= (a0 ++ c0)
+  assert $ (b0 ++ b2) :$= (a1 ++ c1)
+  assert $ (b0 ++ b1) :$= (a2 ++ c2)
+  assert $ (c1 ++ c2) :$= (a0 ++ b0)
+  assert $ (c0 ++ c2) :$= (a1 ++ b1)
+  assert $ (c0 ++ c1) :$= (a2 ++ b2)
+setConstraintsRow _ _ _ = undefined
+
+allAccounted :: [Var] -> FD Int ()
+allAccounted a
+  | length a /= 9 = error $ "Invalid length."
+  | otherwise = do
+      vars <- mapM (newVar . (:[])) [1..9]
+      flip mapM_ a $ \ a -> do
+        assert $ foldl1 (:||) [ a :== n | n <- vars ]
+
+notIn :: [([Var], [Var])] -> FD Int ()
+notIn a = do
+  vars <- mapM (newVar . (:[])) [1..9]
+  mapM_ (notIn' vars) a
+
+notIn' :: [Var] -> ([Var], [Var]) -> FD Int ()
+notIn' vars (a, b) = do
+  flip mapM_ vars $ \ var -> do
+    assert $ foldl1 (:&&) [ a :/= var | a <- a ] :-> foldl1 (:&&) [ b :/= var | b <- b ]
+    assert $ foldl1 (:&&) [ b :/= var | b <- b ] :-> foldl1 (:&&) [ a :/= var | a <- a ]
 
 easy :: Board
 easy = parseBoard $ unlines
@@ -81,5 +174,31 @@ hard112 = parseBoard $ unlines
   , " 8      1"
   , "96   7 5 "
   , "5    98  "
+  ]
+
+garfield3 :: Board
+garfield3 = parseBoard $ unlines
+  [ "  91 63  "
+  , " 6  4 9  "
+  , "1  93 2  "
+  , "4  38    "
+  , " 81 6 42 "
+  , "    94  8"
+  , "  7 19  4"
+  , "  4 2  5 "
+  , "  64 38  "
+  ]
+
+garfield4 :: Board
+garfield4 = parseBoard $ unlines
+  [ "69  8  4 "
+  , "2   5   8"
+  , " 5    2 9"
+  , " 6 7  4 3"
+  , "   431   "
+  , "4 3  5 7 "
+  , "7 2    5 "
+  , "9   1   7"
+  , " 3  6  24"
   ]
 
